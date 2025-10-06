089b0dca547c12d8d44c484391977fa4583c4e76
Jens2014 <jens.kossmagk@gmx.de>
2025-10-06 19:46:15 +0200

    Update Posh-Cisco.psm1
    only header information updated

--- Posh-Cisco.psm1
+++ Posh-Cisco.psm1
@@ -1,5 +1,21 @@
+# Posh-Cisco PowerShell Cisco Module
+# Copyright (c) 2016-2017 Steven Lietaer, All rights reserved.
+#
 # Posh-Cisco PowerShell Cisco Module (shellstream-only robust filters)
-# Maintained by Jens Kossmagk & GPT (2025) | License: GPLv3+
+# Copyright (c) Jens Kossmagk & GPT (2025) | License: GPLv3+
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 Import-Module Posh-SSH
 
